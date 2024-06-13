2d0314f10abd2438ef2dd6de42f7df22e3fe66e8
NgurahFajar <agungfajar476@gmail.com>
2024-06-13 21:13:28 +0700

    comment

--- app/src/main/java/com/example/bullionapp/data/remote/config/ApiConfig.kt
+++ app/src/main/java/com/example/bullionapp/data/remote/config/ApiConfig.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
-class ApiConfig {
+class ApiConfigi{
 
     companion object {
 
@@ -22,6 +22,7 @@ class ApiConfig {
                 .addInterceptor(loggingInterceptor)
                 .build()
 
+            // retrofit
             val retrofit = Retrofit.Builder()
                 .baseUrl("https://api-test.bullionecosystem.com")
                 .addConverterFactory(GsonConverterFactory.create())
